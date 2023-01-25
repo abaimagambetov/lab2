8cf54df0517b842248107a5d9bcdfb43f31b211d
Amir <amirbaymagambetov14@gmail.com>
2023-01-25 11:46:40 +0600

    lab 2

---
+++ while.py
@@ -0,0 +1,24 @@
+# 1
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+# 2
+i = 1
+while i < 6:
+    if i == 3:
+        break
+i += 1
+# 3
+i = 1
+while i < 6:
+    if i == 3:
+        continue
+i += 1
+# 4
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
